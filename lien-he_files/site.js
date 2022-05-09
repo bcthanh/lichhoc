7f1cd41555b0fee5332eb692561f86f98d7c1632
bcthanh@pdu.edu.vn <bcthanh@pdu.edu.vn>
2022-05-09 09:04:12 +0700

    Source phien ban 1

---
+++ lien-he_files/site.js
@@ -0,0 +1,16 @@
+﻿//var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+//(function () {
+//    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+//    s1.async = true;
+//    s1.src = 'https://embed.tawk.to/58d35139f97dd14875f59a76/default';
+//    s1.charset = 'UTF-8';
+//    s1.setAttribute('crossorigin', '*');
+//    s0.parentNode.insertBefore(s1, s0);
+//})();
+
+
+$(".search-modal").on('hidden.bs.modal', function () {
+    $('#Search-Course-Name').val('');
+});
+
+
